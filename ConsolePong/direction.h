5113cc2d91c10ec904fb9307a5f10ecaeeacdf77
Otávio Pace <otaviopp8@gmail.com>
2017-04-30 16:35:18 -0300

    Direction enum added

---
+++ ConsolePong/direction.h
@@ -0,0 +1,15 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum Direction
+{
+	STOP = 0,
+	LEFT = 1,
+	UP_LEFT = 2,
+	DOWN_LEFT = 3,
+	RIGHT = 4,
+	UP_RIGHT = 5,
+	DOWN_RIGHT = 6
+};
+
+#endif // !DIRECTION_H
